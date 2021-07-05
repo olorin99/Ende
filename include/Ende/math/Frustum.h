cc45d92e9955c3b34833e53841ee4147c25f5b38
Christian Strodl <christianstrodl1@gmail.com>
2021-07-05 17:20:44 +1000

    added frustum to math

---
+++ include/Ende/math/Frustum.h
@@ -0,0 +1,35 @@
+//
+// Created by cstro29 on 5/7/21.
+//
+
+#ifndef ENDE_FRUSTUM_H
+#define ENDE_FRUSTUM_H
+
+#include <Ende/math/Vec.h>
+#include <Ende/math/Mat.h>
+
+namespace ende::math {
+
+    class Frustum {
+    public:
+
+        Frustum(const ende::math::Mat4f& matrix);
+
+        void update(const ende::math::Mat4f& matrix);
+
+
+        bool intersect(const ende::math::Vec3f& point) const;
+
+        bool intersect(const ende::math::Vec3f& point, f32 radius) const;
+
+        //TODO: add AABB checking
+
+    private:
+
+        std::array<std::pair<ende::math::Vec3f, f32>, 6> _planes;
+
+    };
+
+}
+
+#endif //ENDE_FRUSTUM_H
---
+++ src/math/Frustum.cpp
@@ -0,0 +1,57 @@
+//
+// Created by cstro29 on 5/7/21.
+//
+
+#include "Ende/math/Frustum.h"
+
+ende::math::Frustum::Frustum(const ende::math::Mat4f &matrix) {
+    update(matrix);
+}
+
+void ende::math::Frustum::update(const ende::math::Mat4f &matrix) {
+    _planes[0].first[0] = matrix[0][3] - matrix[0][0];
+    _planes[0].first[1] = matrix[1][3] - matrix[1][0];
+    _planes[0].first[2] = matrix[2][3] - matrix[2][0];
+    _planes[0].second = matrix[3][3] - matrix[3][0];
+
+    _planes[1].first[0] = matrix[0][3] + matrix[0][0];
+    _planes[1].first[1] = matrix[1][3] + matrix[1][0];
+    _planes[1].first[2] = matrix[2][3] + matrix[2][0];
+    _planes[1].second = matrix[3][3] + matrix[3][0];
+
+    _planes[2].first[0] = matrix[0][3] - matrix[0][1];
+    _planes[2].first[1] = matrix[1][3] - matrix[1][1];
+    _planes[2].first[2] = matrix[2][3] - matrix[2][1];
+    _planes[2].second = matrix[3][3] - matrix[3][1];
+
+    _planes[3].first[0] = matrix[0][3] + matrix[0][1];
+    _planes[3].first[1] = matrix[1][3] + matrix[1][1];
+    _planes[3].first[2] = matrix[2][3] + matrix[2][1];
+    _planes[3].second = matrix[3][3] + matrix[3][1];
+
+    _planes[4].first[0] = matrix[0][3] - matrix[0][2];
+    _planes[4].first[1] = matrix[1][3] - matrix[1][2];
+    _planes[4].first[2] = matrix[2][3] - matrix[2][2];
+    _planes[4].second = matrix[3][3] - matrix[3][2];
+
+    _planes[5].first[0] = matrix[0][3] + matrix[0][2];
+    _planes[5].first[1] = matrix[1][3] + matrix[1][2];
+    _planes[5].first[2] = matrix[2][3] + matrix[2][2];
+    _planes[5].second = matrix[3][3] + matrix[3][2];
+}
+
+bool ende::math::Frustum::intersect(const ende::math::Vec3f &point) const {
+    for (auto& plane : _planes) {
+        if (point.dot(plane.first) + plane.second <= 0)
+            return false;
+    }
+    return true;
+}
+
+bool ende::math::Frustum::intersect(const ende::math::Vec3f &point, f32 radius) const {
+    for (auto& plane : _planes) {
+        if (point.dot(plane.first) + plane.second < -radius)
+            return false;
+    }
+    return true;
+}
