ebd62b5d9994c67b9c875695dbbae354089a3a84
christian <christianstrodl1@gmail.com>
2022-03-30 21:41:56 +1000

    updated profile dump format

--- src/profile/ProfileManager.cpp
+++ src/profile/ProfileManager.cpp
@@ -1,6 +1,7 @@
 
 #include "Ende/profile/ProfileManager.h"
 #include <cstring>
+#include <map>
 
 void ende::profile::submit(ProfileData &&data) {
     ProfileManager::submit(std::forward<ProfileData>(data));
@@ -18,8 +19,31 @@ void ende::profile::ProfileManager::submit(ProfileData &&data) {
     instance()._data.push(std::forward<ProfileData>(data));
 }
 
+struct DumpData {
+    const char* label;
+    i32 line;
+    const char* file;
+    ende::Vector<ende::time::Duration> times;
+};
+
 bool ende::profile::ProfileManager::dump(fs::File& file) {
+
+    std::map<const char*, DumpData> mappedData;
     for (auto& data : instance()._data) {
+        auto it = mappedData.find(data.label);
+        if (it != mappedData.end())
+            it->second.times.push(data.time);
+        else {
+            auto inserted = mappedData.insert(std::make_pair(data.label, DumpData{data.label, data.line, data.file, Vector<time::Duration>()}));
+            inserted.first->second.times.push(data.time);
+        }
+    }
+
+
+
+
+    for (auto& mapped : mappedData) {
+        auto& data = mapped.second;
         file.write({data.label, static_cast<u32>(strlen(data.label))});
         file.write(":");
         file.write({data.file, static_cast<u32>(strlen(data.file))});
@@ -28,8 +52,11 @@ bool ende::profile::ProfileManager::dump(fs::File& file) {
         file.write(line);
         file.write(":");
 
-        auto sec = std::to_string((data.time.milliseconds()));
-        file.write(sec);
+        for (auto& time : data.times) {
+            auto sec = std::to_string((time.nanoseconds()));
+            file.write(sec);
+            file.write(",");
+        }
         file.write("\n");
     }
 
