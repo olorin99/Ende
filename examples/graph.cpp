2683ed3542b9425135270721fddf8cd001475746
olorin99 <christianstrodl1@gmail.com>
2026-01-26 22:02:29 +1000

    Add graph container to abstract vertices and edges.

--- examples/graph.cpp
+++ examples/graph.cpp
@@ -5,6 +5,7 @@
 using namespace ende::graph;
 
 struct BufferEdge : public Edge {};
+struct ImageEdge : public Edge {};
 
 int main() {
 
@@ -30,18 +31,95 @@ int main() {
     vertex2.outputs = { edge4 };
 
 
-    auto graph = std::to_array({
+    auto vertices = std::to_array({
         vertex0, vertex1, vertex2
     });
 
-    const auto graphSpan = std::span(graph.data(), graph.size());
+    const auto graphSpan = std::span(vertices.data(), vertices.size());
 
     auto topological = TRY_MAIN(topologicalSort(graphSpan, edge4, 5));
 
     for (const auto& vertex : topological) {
         printf("%d, ", vertex.id);
     }
+    printf("\n");
 
+
+
+
+    auto graph = Graph<Vertex<ImageEdge, BufferEdge>>();
+    graph.reserveVertices(3);
+
+    auto& v0 = graph.addVertex();
+    auto& v1 = graph.addVertex();
+    auto& v2 = graph.addVertex();
+
+    auto e0 = graph.addEdge<ImageEdge>(v0, v1);
+    graph.addEdge<BufferEdge>(v0, v2);
+    graph.addEdge(v1, v2);
+
+    // auto e0 = graph.addEdge();
+    // auto e1 = graph.addEdge();
+    // auto e2 = graph.addEdge();
+    // auto e3 = graph.addEdge();
+    auto e4 = graph.addEdge();
+
+    // v0.outputs = { e0, e1, e2 };
+
+    // v1.inputs = { e0, e1 };
+    // v1.outputs = { e3 };
+
+    // v2.inputs = { e2, e3 };
+    v2.outputs = { e4 };
+
+    // auto t = TRY_MAIN(topologicalSort(graph.getVertices(), e4, graph.edgeCount()));
+    auto t = TRY_MAIN(graph.sort(e4));
+
+    for (const auto& vertex : t) {
+        printf("%d, ", vertex.id);
+    }
+    printf("\n");
+
+
+
+    {
+        auto g = Graph<Vertex<ImageEdge, BufferEdge>>();
+        g.reserveVertices(10);
+
+        auto& clearMeshletInstances = g.addVertex();
+        auto& cullMeshes = g.addVertex();
+        auto& writeCullMeshletsCommand = g.addVertex();
+        auto& cullMeshlets = g.addVertex();
+        auto& writeDrawCommand = g.addVertex();
+
+        auto& drawMeshlets = g.addVertex();
+        auto& writePrimitives = g.addVertex();
+
+        g.addEdge(clearMeshletInstances, cullMeshes);
+        g.addEdge(cullMeshes, writeCullMeshletsCommand);
+        g.addEdge(writeCullMeshletsCommand, cullMeshlets);
+        g.addEdge(cullMeshlets, writeDrawCommand);
+        g.addEdge(clearMeshletInstances, cullMeshlets);
+
+        if (true) {
+            g.addEdge(writeDrawCommand, writePrimitives);
+            g.addEdge(writePrimitives, drawMeshlets);
+        } else {
+            g.addEdge(writeDrawCommand, drawMeshlets);
+        }
+
+
+        // auto root = g.addEdge();
+        // drawMeshlets.outputs = { root };
+
+        auto sorted = TRY_MAIN(g.sort(drawMeshlets));
+
+        for (const auto& vertex : sorted) {
+            printf("%d, ", vertex.id);
+        }
+        printf("\n");
+
+    }
 
     return 0;
 }
--- include/Ende/graph/graph.h
+++ include/Ende/graph/graph.h
@@ -30,19 +30,20 @@ namespace ende::graph {
         std::vector<Edge> outputs = {};
     };
 
+
     enum class Error {
         IS_CYCLICAL,
 
         EDGE_TYPE_DOESNT_MATCH,
     };
 
-    template <IsEdge... Args>
+    template <IsEdge Edge>
     struct EdgeHelper {
 
         template <std::size_t I = 0>
-        static auto getId(typename Vertex<Args...>::Edge edge) -> std::expected<u32, Error> {
-            if constexpr (I < std::variant_size_v<typename Vertex<Args...>::Edge>) {
-                if (std::holds_alternative<std::variant_alternative_t<I, typename Vertex<Args...>::Edge>>(edge)) {
+        static auto getId(Edge edge) -> std::expected<u32, Error> {
+            if constexpr (I < std::variant_size_v<Edge>) {
+                if (std::holds_alternative<std::variant_alternative_t<I, Edge>>(edge)) {
                     return std::get<I>(edge).id;
                 }
                 return getId<I + 1>(edge);
@@ -50,17 +51,39 @@ namespace ende::graph {
             return std::unexpected(Error::EDGE_TYPE_DOESNT_MATCH);
         }
 
+        template <std::size_t I = 0>
+        static auto setId(Edge edge, u32 id) -> std::expected<Edge, Error> {
+            if constexpr (I < std::variant_size_v<Edge>) {
+                if (std::holds_alternative<std::variant_alternative_t<I, Edge>>(edge)) {
+                    std::variant_alternative_t<I, Edge> e = std::get<I>(edge);
+                    e.id = id;
+                    return e;
+                }
+                return setId<I + 1>(edge, id);
+            }
+            return std::unexpected(Error::EDGE_TYPE_DOESNT_MATCH);
+        }
+
+        template <typename T, std::size_t I = 0>
+        static constexpr auto holdsType() -> bool {
+            if constexpr (I < std::variant_size_v<Edge>) {
+                if constexpr (std::is_same_v<T, std::variant_alternative_t<0, Edge>>) {
+                    return true;
+                }
+                return holdsType<T, I + 1>();
+            }
+            return false;
+        }
+
     };
 
-
-
     template <IsEdge... Args>
-    auto topologicalSort(std::span<Vertex<Args...>> vertices, typename Vertex<Args...>::Edge rootEdge, u32 edgeCount) -> std::expected<std::vector<Vertex<Args...>>, Error>  {
+    auto topologicalSort(std::span<Vertex<Args...>> vertices, std::span<typename Vertex<Args...>::Edge> rootEdges, u32 edgeCount) -> std::expected<std::vector<Vertex<Args...>>, Error> {
         std::vector<std::vector<Vertex<Args...>>> adjacencies = {};
         adjacencies.resize(edgeCount);
         for (auto& vertex : vertices) {
             for (auto& output : vertex.outputs) {
-                auto edgeId = TRY(EdgeHelper<Args...>::getId(output));
+                auto edgeId = TRY(EdgeHelper<typename Vertex<Args...>::Edge>::getId(output));
                 adjacencies[edgeId].push_back(vertex);
             }
         }
@@ -76,7 +99,7 @@ namespace ende::graph {
             visited[vertex.id] = true;
             onStack[vertex.id] = true;
             for (auto& input : vertex.inputs) {
-                auto edgeId = TRY(EdgeHelper<Args...>::getId(input));
+                auto edgeId = TRY(EdgeHelper<typename Vertex<Args...>::Edge>::getId(input));
                 for (auto& adjacent : adjacencies[edgeId]) {
                     if (visited[adjacent.id] && onStack[adjacent.id])
                         return false;
@@ -92,13 +115,95 @@ namespace ende::graph {
             return true;
         };
 
-        auto rootId = TRY(EdgeHelper<Args...>::getId(rootEdge));
-        for (auto& adjacent : adjacencies[rootId]) {
-            if (!TRY(dfs(adjacent))) return std::unexpected(Error::IS_CYCLICAL);
+        for (auto& rootEdge : rootEdges) {
+            auto rootId = TRY(EdgeHelper<typename Vertex<Args...>::Edge>::getId(rootEdge));
+            for (auto& adjacent : adjacencies[rootId]) {
+                if (!TRY(dfs(adjacent))) return std::unexpected(Error::IS_CYCLICAL);
+            }
         }
 
         return topological;
     }
+
+    template <IsEdge... Args>
+    auto topologicalSort(std::span<Vertex<Args...>> vertices, typename Vertex<Args...>::Edge rootEdge, u32 edgeCount) -> std::expected<std::vector<Vertex<Args...>>, Error> {
+        auto rootEdges = std::span(&rootEdge, 1);
+        return topologicalSort(vertices, rootEdges, edgeCount);
+    }
+
+    template <IsEdge... Args>
+    auto topologicalSort(std::span<Vertex<Args...>> vertices, Vertex<Args...> rootVertex, u32 edgeCount) -> std::expected<std::vector<Vertex<Args...>>, Error> {
+        return topologicalSort(vertices, rootVertex.inputs, edgeCount);
+    }
+
+    template <typename V = Vertex<Edge>>
+    class Graph {
+    public:
+
+        using Vertex = V;
+        using Edge = Vertex::Edge;
+
+
+        auto addVertex() -> Vertex& {
+            auto vertex = Vertex();
+            vertex.id = _vertexIndex++;
+            _vertices.emplace_back(vertex);
+            return _vertices.back();
+        }
+
+        template <IsEdge E = Edge>
+        auto addEdge() -> Edge {
+            auto edge = EdgeHelper<Edge>::setId(E(), _edgeIndex++);
+            _edges.emplace_back(*edge);
+            return _edges.back();
+        }
+
+        template <IsEdge E = Edge>
+        auto addEdge(Vertex& parent, Vertex& child) -> Edge {
+            auto edge = addEdge<E>();
+            parent.outputs.emplace_back(edge);
+            child.inputs.emplace_back(edge);
+            return edge;
+        }
+
+        auto getVertices() -> std::span<Vertex> {
+            return std::span(_vertices.data(), _vertices.size());
+        }
+
+        auto getEdges() -> std::span<Edge> {
+            return std::span(_edges.data(), _edges.size());
+        }
+
+        auto vertexCount() -> u32 { return _vertices.size(); }
+        auto edgeCount() -> u32 { return _edges.size(); }
+
+        void reserveVertices(u32 count) {
+            _vertices.reserve(count);
+        }
+
+        void reserveEdges(u32 count) {
+            _edges.reserve(0);
+        }
+
+
+
+        auto sort(Edge rootEdge) -> std::expected<std::vector<Vertex>, Error> {
+            return topologicalSort(getVertices(), rootEdge, edgeCount());
+        }
+
+        auto sort(Vertex rootVertex) -> std::expected<std::vector<Vertex>, Error> {
+            return topologicalSort(getVertices(), rootVertex, edgeCount());
+        }
+
+    private:
+
+        std::vector<Vertex> _vertices = {};
+        std::vector<Edge> _edges = {};
+
+        u32 _vertexIndex = 0;
+        u32 _edgeIndex = 0;
+
+    };
 }
 
 #endif //ENDE_GRAPH_H
